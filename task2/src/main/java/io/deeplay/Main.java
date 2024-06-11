30b2180b45b09df833072927c97c9eecf1ad42bc
fatalistix <slavabal11@outlook.com>
2024-06-11 19:50:19 +0700

    added simple ui

--- task2/src/main/java/io/deeplay/Main.java
+++ task2/src/main/java/io/deeplay/Main.java
@@ -1,19 +1,37 @@
 package io.deeplay;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[10];
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите размер массива:");
+            System.out.print("-> ");
 
-        ArrayRandomFiller arrayRandomFiller = new ArrayRandomFiller();
-        ArrayPrinter arrayPrinter = new ArrayPrinter();
-        MostPopularNumberFinder mostPopularNumberFinder = new MostPopularNumberFinder();
+            int arraySize = scanner.nextInt();
+            int[] array = new int[arraySize];
 
-        arrayRandomFiller.fillWithRandomNumbers(array, -2, 2);
+            ArrayRandomFiller arrayRandomFiller = new ArrayRandomFiller();
+            ArrayPrinter arrayPrinter = new ArrayPrinter();
+            MostPopularNumberFinder mostPopularNumberFinder = new MostPopularNumberFinder();
 
-        arrayPrinter.printArray(array);
+            System.out.println("Введите через пробел левую и правую границы диапазона случайных чисел:");
+            System.out.print("-> ");
 
-        int[] mostPopularNumbers = mostPopularNumberFinder.findMostPopularNumbers(array);
+            int leftBound = scanner.nextInt();
+            int rightBound = scanner.nextInt();
 
-        arrayPrinter.printArray(mostPopularNumbers);
+            arrayRandomFiller.fillWithRandomNumbers(array, leftBound, rightBound);
+
+            System.out.println("Массив был заполнен следующими числами:");
+            arrayPrinter.printArray(array);
+
+            int[] mostPopularNumbers = mostPopularNumberFinder.findMostPopularNumbers(array);
+
+            System.out.println("Следующие числа встретились в массиве наибольшее количество раз:");
+            arrayPrinter.printArray(mostPopularNumbers);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
