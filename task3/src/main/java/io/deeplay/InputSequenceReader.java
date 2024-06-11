8b7cd5c6f27762c66c40f96cbef87febc70f402d
fatalistix <slavabal11@outlook.com>
2024-06-11 18:27:25 +0700

    fixed bug with closing scanner

--- task3/src/main/java/io/deeplay/InputSequenceReader.java
+++ task3/src/main/java/io/deeplay/InputSequenceReader.java
@@ -1,25 +1,17 @@
 package io.deeplay;
 
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class InputSequenceGenerator {
+public class InputSequenceReader {
 
-    public int[] generateSequence(int size) throws InvalidSequenceException {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int[] sequence = new int[size];
-            for (int i = 0; i < size; ++i) {
-                sequence[i] = scanner.nextInt();
-                if (sequence[i] < 1 || sequence[i] > 6) {
-                    throw new InvalidSequenceException("Введено некорректное число");
-                }
+    public int[] readSequence(int size, Scanner scanner) throws InvalidSequenceException {
+        int[] sequence = new int[size];
+        for (int i = 0; i < size; ++i) {
+            sequence[i] = scanner.nextInt();
+            if (sequence[i] < 1 || sequence[i] > 6) {
+                throw new InvalidSequenceException("Введено некорректное число");
             }
-            return sequence;
-        } catch (InputMismatchException e) {
-            throw new InvalidSequenceException("Введено некорректное число");
-        } catch (NoSuchElementException e) {
-            throw new InvalidSequenceException("Недостаточно чисел для генерации последовательности");
         }
+        return sequence;
     }
 }
