cf6e05f53fd5da409c8b074af653701eb0cf17bf
fatalistix <slavabal11@outlook.com>
2024-06-11 11:48:47 +0700

    added main function as an example

--- task2/src/main/java/io/deeplay/Main.java
+++ task2/src/main/java/io/deeplay/Main.java
@@ -2,6 +2,18 @@ package io.deeplay;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = new int[10];
+
+        ArrayRandomFiller arrayRandomFiller = new ArrayRandomFiller();
+        ArrayPrinter arrayPrinter = new ArrayPrinter();
+        MostPopularNumberFinder mostPopularNumberFinder = new MostPopularNumberFinder();
+
+        arrayRandomFiller.fillWithRandomNumbers(array, -2, 2);
+
+        arrayPrinter.printArray(array);
+
+        int[] mostPopularNumbers = mostPopularNumberFinder.findMostPopularNumbers(array);
+
+        arrayPrinter.printArray(mostPopularNumbers);
     }
 }
